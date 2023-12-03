4c70f66cd700800ad1f7236abdf9b237ad089994
Gabriel <12880227-igr88@users.noreply.gitlab.com>
2023-12-03 13:49:31 -0600

    Initial commit

---
+++ src/main/java/me/dev/interview/App.java
@@ -0,0 +1,13 @@
+package me.dev.interview;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
